a030ebc5865e59ba012bbd1f87e1b5e79eface95
Tsukuni <tamkchi.fugu@gmail.com>
2018-08-04 01:35:09 +0900

    Plan Model・Controller作成

---
+++ kenta/app/controllers/plans_controller.rb
@@ -0,0 +1,49 @@
+class PlansController < ApplicationController
+  before_action :set_plan, only: %i[show update destroy]
+
+  def index
+    @plans = set_user.plans
+    render json: @plans
+  end
+
+  def show
+    render json: @plan
+  end
+
+  def create
+    plan = Plan.new(plan_params)
+    plan.user = set_user
+    if plan.save!
+      render json: plan, status: 201
+    else
+      render json: { errors: plan.errors }, status: 422
+    end
+  end
+
+  def update
+    if @plan.update(plan_params)
+      render json: @plan, status: 200
+    else
+      render json: { errors: plan.errors }, status:422
+    end
+  end
+
+  def destroy
+    @plan.destroy
+    head 204
+  end
+
+  private
+
+  def set_user
+    User.find(params[:user_id])
+  end
+
+  def set_plan
+    @plan = Plan.find(params[:id])
+  end
+
+  def plan_params
+    params.permit(:date, :place, :drink, :course)
+  end
+end
--- kenta/app/models/user.rb
+++ kenta/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :plans, dependent: :destroy
   validates :name, presence: true
   validates :sex, presence: true
   validates :mail, presence: true, uniqueness: true

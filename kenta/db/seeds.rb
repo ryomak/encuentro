bc72b67b2f1aa73f761de7518d8a8c238ed3a901
Tsukuni <tamkchi.fugu@gmail.com>
2018-08-04 01:27:11 +0900

    Planサンプルデータ挿入

--- kenta/db/seeds.rb
+++ kenta/db/seeds.rb
@@ -11,4 +11,11 @@ ActiveRecord::Base.transaction do
   10.times do |i|
     User.find_or_initialize_by({ name:  "松岡裕典#{i}" }).update!({ name: "松岡裕典#{i}", sex: "female", mail: "sample#{i}@sample.com", birthday: "1996/#{i}/05", university: "同志社" });
   end
+
+  #Planサンプル
+  5.times do |i|
+    user = User.find(1)
+    plan = user.plans.find_or_initialize_by({ date: "2018/10/#{i+1}" })
+    plan.update!({ date: "2018/10/#{i+1}", place: "ラブホテル", drink: "あり", course: "おっぱいコース" });
+  end
 end

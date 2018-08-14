ab744dd9a4f9954f5f1cf39f74153d0f633ac7ea
Tsukuni <tamkchi.fugu@gmail.com>
2018-08-14 09:41:51 +0900

    管理用api作成

---
+++ kenta/app/controllers/api/v1/admin/plans_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::Admin::PlansController < ApplicationController
+  before_action :authoritative
+  before_action :admin_user?
+  before_action :set_plan, only: %i[show update destroy]
+
+  def index
+    plans = Plan.all
+    render json: plans
+  end
+
+  def show
+    render json: @plan
+  end
+
+  def update
+    if @plan.update(plan_params)
+      render json: @plan, status: 200
+    else
+      render json: { errors: plan.errors }, status:422
+    end
+  end
+
+  def destroy
+    @plan.destroy
+    head 204
+  end
+
+  private
+
+  def set_plan
+    @plan = Plan.find(params[:id])
+  end
+
+  def plan_params
+    params.permit(:date, :place, :drink, :course, :status)
+  end
+
+end

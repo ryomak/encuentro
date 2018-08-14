3b829afc586a3c9dc54611e5168612482d932930
Tsukuni <tamkchi.fugu@gmail.com>
2018-08-14 09:40:54 +0900

    routes 変更

--- kenta/config/routes.rb
+++ kenta/config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
       post 'login' => 'user_token#create'
       post 'signup' => 'user#create'
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-      resource :user , only: [:show, :update, :destroy] do
+      resource :user, only: [:show, :update, :destroy] do
         resources :plans
+      end
+      namespace :admin do
+        resources :users, only: [:index] do
+          resources :plans, only: [:index, :show, :update, :destroy], shallow: true
+        end
       end
     end
   end

beef7a39cc55eedc8de064c05fb628ad3ca74d58
Tsukuni <tamkchi.fugu@gmail.com>
2018-08-26 14:26:51 +0900

    rename

--- server/db/fixtures/development/create_admin_user.rb
+++ server/db/fixtures/development/create_admin_user.rb
@@ -1,11 +1,10 @@
-User.seed(:id,
-  {
+User.create!({
     name: 'admin',
-    sex:'male',
-    email: 'admin@sample.com',
+    sex:'female',
+    email: 'sample_admin@sample.com',
     password: 'admin',
     password_confirmation: 'admin',
     university: 'けんたチュキチュキクラブ',
     birthday: '1988/06/11',
     status: true
-})
+  })

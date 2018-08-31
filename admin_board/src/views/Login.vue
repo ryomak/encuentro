c79cecb8cf01380ad979b8da765b201ad2a8aa98
Tsukuni <tamkchi.fugu@gmail.com>
2018-08-31 17:09:24 +0900

    一時的

---
+++ admin_board/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-form>
+      <b-form-group label="Email address:"
+                    label-for="emailForm">
+        <b-form-input id="emailForm"
+                      type="email"
+                      required
+                      placeholder="Enter email" />
+      </b-form-group>
+      <b-form-group label="Password:"
+                    label-for="passwordForm">
+        <b-form-input id="passwordForm"
+                      type="password"
+                      required
+                      placeholder="Enter password" />
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </div>
+</template>
